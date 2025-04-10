c9b4d882b032d32d7f3ccd538d828867c75c4491
Lucas Kageyama <lnakamura.kage@gmail.com>
2025-04-10 13:27:44 -0800

    store models

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', include('store.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
