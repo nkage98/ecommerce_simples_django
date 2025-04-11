d2c67dde9c4d5001d788a29422dbc43e24011379
Lucas Kageyama <lnakamura.kage@gmail.com>
2025-04-10 16:08:46 -0800

    template

--- store/models.py
+++ store/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 #categorias
 class Category(models.Model):
@@ -21,7 +22,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=250, default='', black=True, null=True)
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
     
     def __str__(self):
