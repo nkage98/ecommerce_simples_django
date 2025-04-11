afd66df400f3cd477cd0ca1b3eb4f485e5adcb58
Lucas Kageyama <lnakamura.kage@gmail.com>
2025-04-10 23:15:54 -0800

    adjusting html

--- store/views.py
+++ store/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
-    context = {}
+    products = Product.objects.all()
+
+    context = {
+        'products':products
+    }
     return render(
         request, 'home.html', context,
     )
