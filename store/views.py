4afcd9cb89db948c58fd5b1964c56eee4b88b268
Lucas Kageyama <lnakamura.kage@gmail.com>
2025-04-12 13:08:23 -0800

    user register page

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 def home(request):
     products = Product.objects.all()
@@ -35,6 +39,25 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out"))
     return redirect('home')
+
+def register_user(request):
+        form = SignUpForm()
+        if request.method == "POST":
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                messages.success(request, ("You have been registered"))
+                return redirect('home')
+            else:
+                messages.success(request, ("Something went wrong, please register again"))
+                return redirect('register')
+        else:
+            return render(request, 'register.html', {'form': form})
+        
 
 
 
