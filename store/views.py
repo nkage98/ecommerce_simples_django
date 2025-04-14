7b8cd110d044e57e4b80e2bfd4530df53c1cd2ff
Lucas Kageyama <lnakamura.kage@gmail.com>
2025-04-14 13:25:46 -0800

    add category

--- store/views.py
+++ store/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render, redirect
-from .models import Product
+from .models import Product, Category
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .forms import SignUpForm
 from django import forms
+
+def category(request, foo):
+    foo = foo.replace('-', ' ')
+
+    try:
+        category = Category.object.get(name=foo)
+        products = Product.objects.filter(category=category)
+        return render(request, 'category.html', {'products': products}, {'category': category})
+    
+    except:
+        messages.success(request, ("Category doesn't exist"))
+        return redirect('home')
+
+
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+                                  
+    context = {
+        'product':product
+    }
+    return render(
+        request, 'product.html', context,
+    )
+
 
 def home(request):
     products = Product.objects.all()
@@ -17,8 +41,10 @@ def home(request):
         request, 'home.html', context,
     )
 
+
 def about(request):
     return render(request, 'about.html', {})
+
 
 def login_user(request):
     if request.method == 'POST':
@@ -35,10 +61,12 @@ def login_user(request):
     else:
         return render(request, 'login.html', {})
 
+
 def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out"))
     return redirect('home')
+
 
 def register_user(request):
         form = SignUpForm()
